3bdc1bc22534b817747471378d7ea6f5d92ab34c
Karthi-DS <kartheesvaranronaldo@gmail.com>
2025-02-11 23:25:51 +0530

    update database connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,19 @@
 const { Sequelize } = require('sequelize');
 
-
-const sequelize = new Sequelize('grading_reports', 'postgres', 'PostgreSQL', {
-    host: 'localhost',
-    dialect: 'postgres', 
-    logging: false, 
-    port: 5432
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Needed for Render
+    },
+  },
+  retry: {
+    match: [/ECONNREFUSED/],
+    max: 5, // Retry up to 5 times
+    backoffBase: 1000, // Start with 1-second delay
+  },
 });
 
 module.exports = sequelize;
