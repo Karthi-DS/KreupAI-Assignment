19e9bd7c3cdf3946664a347eefd058c22e30db6b
Karthi-DS <kartheesvaranronaldo@gmail.com>
2025-03-05 18:33:07 +0530

    fix route

--- backend/routes/examGradesRoutes.js
+++ backend/routes/examGradesRoutes.js
@@ -3,6 +3,6 @@ const { addGrade,getGrade } = require('../controllers/examGradesController');
 
 const router = express.Router();
 router.post('/add-grades', addGrade);
-router.post('/get-grades', getGrade);
+router.get('/get-grades', getGrade);
 
 module.exports = router;
